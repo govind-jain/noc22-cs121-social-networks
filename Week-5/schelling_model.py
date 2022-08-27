6afc98a825616986bc73276e128267deb15b6f9b
Govind Jain <hello@govindjain.com>
2022-08-27 14:22:46 +0530

    Assign types to cells and printing graph
    Assign the cells among empty, Type-1, and Type-2. Then we get colors_dict based on the type of cell. Then we print the multicoloured graph.

--- Week-5/schelling_model.py
+++ Week-5/schelling_model.py
@@ -1,8 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random as rn
 
 
-def get2DGraph(N):
+def get_2D_graph(N):
     G = nx.grid_2d_graph(N, N)
     pos_dict = dict((n, n) for n in G.nodes())
     labels_dict = dict(((i, j), (i * N + j)) for (i, j) in G.nodes())
@@ -10,13 +11,39 @@ def get2DGraph(N):
     return G, pos_dict, labels_dict
 
 
-def plot2DGraph(G, pos_dict, labels_dict):
+def get_colors_dict(G):
+    colors_dict = []
+
+    for each in G.nodes():
+        node_type = G._node[each]['type']
+
+        if node_type == 0:
+            colors_dict.append('#00BFFF')
+        elif node_type == 1:
+            colors_dict.append('#00FF00')
+        else:
+            colors_dict.append('#FF0000')
+
+    return colors_dict
+
+
+def plot_2D_graph(G, pos_dict, labels_dict):
     nx.draw(G, pos_dict, with_labels=0)
     nx.draw_networkx_labels(G, pos_dict, labels=labels_dict)
+
     plt.show()
 
 
-def connectDiagonals2DGraph(G, N):
+def plot_2D_graph_colors(G, pos_dict, labels_dict):
+    colors_dict = get_colors_dict(G)
+
+    nx.draw(G, pos_dict, node_color=colors_dict, with_labels=0)
+    nx.draw_networkx_labels(G, pos_dict, labels=labels_dict)
+
+    plt.show()
+
+
+def connect_diagonals_2D_graph(G, N):
     for (x, y) in G.nodes():
         if x+1 < N:
             if y+1 < N:
@@ -27,8 +54,32 @@ def connectDiagonals2DGraph(G, N):
     return G
 
 
+def assign_type(G):
+    empty_cells_list = []
+    type1_node_list = []
+    type2_node_list = []
+
+    for cell in G.nodes():
+        cell_type = rn.randint(0, 2)
+        G._node[cell]['type'] = cell_type
+
+        if cell_type == 0:
+            empty_cells_list.append(cell)
+        elif cell_type == 1:
+            type1_node_list.append(cell)
+        else:
+            type2_node_list.append(cell)
+
+    return G, empty_cells_list, type1_node_list, type2_node_list
+
+
 N = 10
-G, pos_dict, labels_dict = get2DGraph(N)
-plot2DGraph(G, pos_dict, labels_dict)
-connectDiagonals2DGraph(G, N)
-plot2DGraph(G, pos_dict, labels_dict)
+
+G, pos_dict, labels_dict = get_2D_graph(N)
+# plot_2D_graph(G, pos_dict, labels_dict)
+
+connect_diagonals_2D_graph(G, N)
+# plot_2D_graph(G, pos_dict, labels_dict)
+
+assign_type(G)
+plot_2D_graph_colors(G, pos_dict, labels_dict)
