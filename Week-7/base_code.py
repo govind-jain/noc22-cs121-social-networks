bf5cd84e6746de09f1a3e315a953efdf0dd451dd
Govind Jain <hello@govindjain.com>
2022-09-24 18:05:06 +0530

    Compute next until stability
    Recompute next iteration until all the nodes have same interest of we have crossed a given number of iteration.

--- Week-7/base_code.py
+++ Week-7/base_code.py
@@ -88,12 +88,54 @@ def recalculate_options(G):
     return G
 
 
+def all_nodes_same_action(G):
+
+    n = G.order()
+    nodes = G.nodes()
+
+    count_A = 0
+
+    for each in nodes:
+        if G._node[each]['action'] == 'A':
+            count_A += 1
+
+    if count_A==n or count_A==0:
+        return True
+
+    return False
+
+
 def main():
     G = nx.erdos_renyi_graph(10, 0.5)
     G = set_all_A(G)
 
     listB = [0, 1]
     G = set_selected_B(G, listB)
+
+    print_graph(G)
+
+    count = 0
+    flag = False
+
+    while(1):
+
+        if count >= 100:
+            break
+
+        flag = all_nodes_same_action(G)
+
+        if flag:
+            break
+
+        G = recalculate_options(G)
+        # print_graph(G)
+
+        count += 1
+
+    if flag:
+        print("Cascade is completed")
+    else:
+        print("Cascade is not completed")
 
     print_graph(G)
 
