c3d827dde8edda42633b0708a9e5ea41fe495521
Govind Jain <hello@govindjain.com>
2022-09-24 18:02:55 +0530

    Compute interest for next iteration
    Recompute the interest of each node based on the payoffs and the interest of their friends.

--- Week-7/base_code.py
+++ Week-7/base_code.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import random as rn
 
 def set_all_A(G):
 
@@ -35,6 +37,55 @@ def print_graph(G):
 
     nx.draw(G, with_labels=True, node_color=colors, node_size=800)
     plt.show()
+
+
+def find_neighbour(id, char, G):
+    
+    count = 0
+    neighbors = G.neighbors(id)
+
+    for adj in neighbors:
+        if G._node[adj]['action'] == char:
+            count += 1
+    
+    return count
+
+
+def next_iteration_values(G):
+
+    nodes = G.nodes()
+
+    payoff_A = 5
+    payoff_B = 12
+
+    new_results = {}
+
+    for each in nodes():
+        count_A = find_neighbour(each, 'A', G)
+        count_B = find_neighbour(each, 'B', G)
+
+        overall_payoff_A = count_A * payoff_A
+        overall_payoff_B = count_B * payoff_B
+
+        if overall_payoff_A > overall_payoff_B:
+            new_results[each] = 'A'
+        elif overall_payoff_A < overall_payoff_B:
+            new_results[each] = 'B'
+        else:
+            new_results[each] = rn.choice(['A', 'B'])
+
+    return new_results
+
+
+def recalculate_options(G):
+
+    new_results = next_iteration_values(G)
+    nodes = G.nodes()
+
+    for each in nodes:
+        G._node[each]['action'] = new_results[each]
+    
+    return G
 
 
 def main():
