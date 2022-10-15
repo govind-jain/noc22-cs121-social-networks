6b103f9bddbaa0f707b59a7f5854725bc1212b00
Govind Jain <hello@govindjain.com>
2022-10-15 16:38:25 +0530

    Implement homophily among nodes
    Add nodes between neighbour nodes. The size of neighbour is taken as input.

--- Week-12/small_world_network.py
+++ Week-12/small_world_network.py
@@ -29,3 +29,18 @@ def add_weak_ties(G, nodeList, number_of_rewirings_to_be_done):
         G = add_weak_tie(G, nodeList)
 
     return G
+
+
+def add_edges_homophily(G, nodes, farthestNeighborDistance):
+    
+    n = G.number_of_nodes()
+    
+    if farthestNeighborDistance >= n/2:
+        farthestNeighborDistance = n/2
+
+    for idx in range(0, n):
+        for offset in range(1, farthestNeighborDistance + 1):
+            G.add_edge(nodes[idx], nodes[(idx+offset)%n])
+            G.add_edge(nodes[idx], nodes[(idx-offset+n)%n])
+
+    return G
